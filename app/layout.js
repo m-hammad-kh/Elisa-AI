f28b06b08042009cc65db36b50ceb639c9edd39b
m-hammad-kh <hammadkhalid0017@gmail.com>
2026-04-10 19:50:48 +0500

    Update page title to ELISA AI - WEBSITE BUILDER

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import CustomCursor from "@/components/custom/CustomCursor";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ELISA AI",
+  title: "ELISA AI - WEBSITE BUILDER",
   description: "Build production-ready applications in minutes with Elisa AI.",
 };
 
